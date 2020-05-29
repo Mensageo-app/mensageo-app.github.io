268dd088f45490820cc62492902f82f3639c0e76
Reynau <xavi.lopez.reynau@gmail.com>
2020-05-29 16:36:30 +0200

    Initial commit

---
+++ precache-manifest.b16d2b6e29abfd07a3aa973001208c4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2749e48c96f4051d31f41006b45f35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a475e63d2ccd0e484c89",
+    "url": "/static/js/2.a5ec25ea.chunk.js"
+  },
+  {
+    "revision": "2eb049c4e05d87008457beac921e56da",
+    "url": "/static/js/2.a5ec25ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c76d4c629ee13c665d",
+    "url": "/static/js/main.8d9d0bcb.chunk.js"
+  },
+  {
+    "revision": "a7778f5ef91555148848",
+    "url": "/static/js/runtime-main.03a55a0a.js"
+  }
+]);
